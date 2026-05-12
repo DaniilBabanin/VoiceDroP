9805cf9e2058eeb83a9119f52bceef6f594f9f38
Daniil Babanin <daniil@babanin.de>
2026-05-12 16:15:40 +0000

    fix: add missing transport field to MessageRepositoryTest message helper

--- app/src/test/kotlin/com/voicedrop/storage/MessageRepositoryTest.kt
+++ app/src/test/kotlin/com/voicedrop/storage/MessageRepositoryTest.kt
@@ -49,6 +49,7 @@ class MessageRepositoryTest {
             contactId = contactId,
             direction = direction,
             state = MessageEntity.STATE_OUTBOX,
+            transport = MessageEntity.TRANSPORT_UNKNOWN,
             encryptedFilePath = null,
             durationMs = 0,
             deleteAfterMs = 0L,
