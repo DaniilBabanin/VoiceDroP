6474835faacd618073fded4625c3f3f178e9f159
Daniil Babanin <daniil@babanin.de>
2026-05-14 19:16:12 +0000

    chore: bump versionCode to 40 for v1.0.1.0-pre.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.voicedrop"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
+        versionCode = 40
         versionName = "1.0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
